d2f69915f24ad5f33985c1646969907241f4d2ae
Mathias Schoettle <schoettlemathias@gmail.com>
2025-01-18 12:30:40 +0100

    add nuxt ui

--- app.vue
+++ app.vue
@@ -1,3 +1,34 @@
+<script setup lang="ts">
+	const isOpen = ref(false)
+</script>
+
 <template>
-	Hello World!
+	<UContainer>
+		<div class="w-full h-screen flex justify-center items-center">
+			<UButton color="blue" label="Open" @click="isOpen = true" />
+			<UModal v-model="isOpen" :transition="false">
+				<div class="p-4">
+					<UCard>
+						<template #header>
+							<div class="flex items-center justify-between">
+								<h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
+									Modal
+								</h3>
+								<UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="isOpen = false" />
+							</div>
+						</template>
+
+						<Placeholder class="h-32" />
+
+						<template #footer>
+							<div class="flex items-center justify-end gap-2">
+								<UButton @click="isOpen = false" color="gray">Accept</UButton>
+								<UButton @click="isOpen = false" color="red">Close</UButton>
+							</div>
+						</template>
+					</UCard>
+				</div>
+			</UModal>
+		</div>
+	</UContainer>
 </template>
