603d1e5f5f70760286b4547c42233220cea650d0
Mathias Schoettle <schoettlemathias@gmail.com>
2025-01-19 16:46:29 +0100

    added pinia dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
- compatibilityDate: '2024-11-01',
- devtools: { enabled: true },
- typescript: { typeCheck: true },
- modules: ['@nuxt/ui'],
-routeRules: {
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	typescript: { typeCheck: true },
+	modules: ['@nuxt/ui', '@pinia/nuxt'],
+	routeRules: {
 		'/': {redirect: '/list'}
 	},
 })
