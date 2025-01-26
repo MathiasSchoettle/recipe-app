064a7bb696eb4b7a0a01408802833c24bb3c8864
Mathias Schoettle <schoettlemathias@gmail.com>
2025-01-26 23:54:04 +0100

    refactored recipe card

--- components/RecipeCard.vue
+++ components/RecipeCard.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-
-import { LazyUProgress, RecipeCard } from '#components';
 import { _backgroundImage, _backgroundPosition } from '#tailwind-config/theme';
 import type { Recipe } from '@/stores/recipes'
 
@@ -14,36 +12,21 @@ const styles = {
 	backgroundPosition: 'center'
 }
 
-function formatDuration(seconds: number): string {
-	// Calculate hours, minutes, and remaining seconds
-	const hours = Math.floor(seconds / 3600);
-	const minutes = Math.floor((seconds % 3600) / 60);
-
-	// Build the formatted string
-	const parts: string[] = [];
-	if (hours > 0) {
-		parts.push(`${hours} hour${hours > 1 ? 's' : ''}`);
-	}
-	if (minutes > 0 || hours === 0) { // Always show minutes if hours are 0
-		parts.push(`${minutes} minute${minutes > 1 ? 's' : ''}`);
-	}
-
-	return parts.join(' ')
-}
-
 const timeString = computed(() => {
-	return formatDuration(props.recipe.time)
+	return formatMinutes(props.recipe.minutes)
 })
 
 </script>
 
 <template>
-	<div class="w-full bg-neutral-800 rounded-md flex gap-5 p-2 overflow-hidden border border-neutral-700 shadow-lg cursor-pointer">
+	<div class="w-full bg-neutral-800 rounded-md flex gap-5 p-1.5 border border-neutral-700 shadow-lg cursor-pointer">
 
 		<div :style="styles" class="shadow-md shadow-black/10 rounded-lg size-28 min-w-28 bg-neutral-500"></div>
 
-		<div  class="flex-1 h-fill flex gap-1.5 pt-1 flex-col">
-			<div class="font-bold">{{ recipe.title }}</div>
+		<div  class="flex-1 h-fill flex gap-1.5 pt-1 flex-col overflow-hidden">
+			<div class="font-bold whitespace-nowrap text-ellipsis overflow-hidden">
+				{{ recipe.title }}
+			</div>
 
 			<div class="flex gap-1 flex-wrap h-11 overflow-hidden">
 				<UBadge v-for="ingredient in recipe.ingredients" class="w-fit h-5" color="gray" size="xs" variant="solid">
