b3417e32a5b8868a92f446a9bcf9477afe971977
Mathias Schoettle <schoettlemathias@gmail.com>
2025-01-26 23:53:47 +0100

    fixed client errors

--- pages/list.vue
+++ pages/list.vue
@@ -18,18 +18,20 @@ const store = useRecipeStore();
 			/>
 		</div>
 
-		<div class="overflow-scroll h-full flex flex-col gap-3 p-5 pt-3">
-			<div v-if="store.hasData" v-for="recipe in store.recipes">
-				<NuxtLink :to="'recipe-' + recipe.id">
-					<RecipeCard :recipe="recipe"/>
-				</NuxtLink>
-			</div>
-			<div v-else>
-				<div class="w-full flex justify-center">
-					<UIcon name="i-lucide-loader" class="animate-spin size-10 text-neutral-500"/>
+		<ClientOnly>
+			<div class="overflow-scroll h-full flex flex-col gap-3 p-3 pt-3">
+				<div v-if="store.hasData" v-for="recipe in store.recipes">
+					<NuxtLink :to="'recipe-' + recipe.id">
+						<RecipeCard :recipe="recipe"/>
+					</NuxtLink>
+				</div>
+				<div v-else>
+					<div class="w-full flex justify-center">
+						<UIcon name="i-lucide-loader" class="animate-spin size-10 text-neutral-500"/>
+					</div>
 				</div>
 			</div>
-		</div>
+		</ClientOnly>
 
 	</div>
 </template>
